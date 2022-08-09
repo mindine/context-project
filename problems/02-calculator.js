8e6e91d75cbf01f5820a21ed314e64b5804ad8d8
zoobob5 <bwin.cr7@gmail.com>
2022-08-08 21:41:52 -0700

    yours for 3

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,5 +1,29 @@
-// Your code here
+class Calculator{
+	constructor(total = 0){
+		this.total = total;
+	}
 
+	add(num){
+		this.total = this.total + num;
+		return this.total;
+	}
+
+	subtract(num){
+		this.total = this.total - num;
+		return this.total;
+	}
+
+	divide(num){
+		this.total = this.total / num;
+		return this.total;
+	}
+
+	multiply(num){
+		this.total = this.total * num;
+		return this.total;
+	}
+
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
