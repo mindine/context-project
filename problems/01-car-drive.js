431b29400a0ad89c6fe975c6d92c8c96223f295b
mindine <agheutatoo@gmail.com>
2022-08-08 23:59:30 -0500

    03 make dogg

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -12,28 +12,6 @@ class Car {
 	}
 }
 
-// const tCAr = new Car(2);
-
-// console.log(tCAr.drive());
-
-
-
-let car = new Car();
-car.drive(0);     // => returns 0
-console.log(car); // => { speed: 0 }
-
-car.drive(10);    // => returns 10
-console.log(car); // => { speed: 10, }
-
-car.drive(50);    // => returns 50
-console.log(car); // -> { speed: 50 }
-
-car.drive(100);   // => returns 100
-console.log(car); // -> { speed: 100 }
-
-
-
-
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,4 +1,25 @@
 // Your code here
+class Dog {
+	constructor(name) {
+		this.name = name;
+	}
+
+	static makeJet() {
+		const dog = new Dog('Jet');
+		return dog;
+	}
+
+	changeName(newName) {
+		this.name = newName;
+		return this.name;
+	}
+
+	speak(word) {
+		return `${this.name} says ${word}`;
+	}
+}
+
+
 
 
 /*****************************************************************************/
