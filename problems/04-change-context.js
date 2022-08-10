712af071873c40e66025075fd01f8135200b2279
mindine <agheutatoo@gmail.com>
2022-08-09 22:08:14 -0500

    04 change context done

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,24 @@
 function changeContext(func, obj) {
   // Your code here
+  let res = func.bind(obj);
+  return res();
 }
+
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
+
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
